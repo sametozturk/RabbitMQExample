f7859c525bd18761dab44e4006ff4cac4e6bf6d4
M.Samet Öztürk <MS@DESKTOP-8H7N5L5>
2023-08-20 19:19:29 +0300

    Add project files.

---
+++ RBConsumer/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory=new ConnectionFactory();
+factory.HostName="localhost";
+
+var connection=factory.CreateConnection();
+var channel=connection.CreateModel();
+
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received +=(model,ea)=>{
+    var byteMessage=ea.Body.ToArray();
+    var message=Encoding.UTF8.GetString(byteMessage);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(queue:"hello",autoAck:true,consumer:consumer);
+
+Console.ReadKey();
---
+++ RBProducer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+
+var connactionFactory=new ConnectionFactory(){
+    HostName="localhost"
+};
+
+var connection=connactionFactory.CreateConnection();
+var channel=connection.CreateModel();
+
+channel.QueueDeclare(queue:"hello",durable:false,exclusive:false,autoDelete:false,arguments:null);
+
+var message="Hello RabbitMQ";
+
+var byteMessage=Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"",routingKey:"hello",basicProperties:null,body:byteMessage);
+
+Console.WriteLine("Mesaj gönderildi");
+
+Console.ReadKey();
